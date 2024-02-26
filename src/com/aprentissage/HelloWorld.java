0bda2ea247b78b16bddc7ada42a5adbdfc3c51e8
Leo <leodelmas31@gmail.com>
2024-02-26 15:25:01 +0100

    exo

--- src/com/aprentissage/HelloWorld.java
+++ src/com/aprentissage/HelloWorld.java
@@ -1,10 +1,14 @@
 package com.aprentissage;
 
 public class HelloWorld {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("bonjour");
-	}
-
+    
+    public static void main(String[] args) {
+        int i;
+		for (i = 0; i < 5; i++) {
+			System.out.println("Bienvenue dans Eclipse !");
+		}
+		
+		System.out.println("C'est la fin !");
+    }
 }
+
