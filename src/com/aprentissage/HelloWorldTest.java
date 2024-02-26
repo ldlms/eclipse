2f12aadaf668dcb3f46c1db0322023fccf91cb63
Leo <leodelmas31@gmail.com>
2024-02-26 16:38:25 +0100

    exo

---
+++ src/com/aprentissage/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.aprentissage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
