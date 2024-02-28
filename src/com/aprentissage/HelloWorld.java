bc8eace055650a271e5f0ca9cc50930422d3d5da
Leo <leodelmas31@gmail.com>
2024-02-28 14:41:14 +0100

    types

--- src/com/aprentissage/HelloWorld.java
+++ src/com/aprentissage/HelloWorld.java
@@ -3,14 +3,23 @@ package com.aprentissage;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		int i;
-		for (i = 0; i < 5; i++) {
-			System.out.println("Bienvenue dans Eclipse !");
-		}
+		// int i;
+		// for (i = 0; i < 5; i++) {
+		// System.out.println("Bienvenue dans Eclipse !");
+		// }
 
-		System.out.println("C'est la fin !");
-		HelloWorld newHelloWorld = new HelloWorld();
-		newHelloWorld.add(5, 4);
+		// System.out.println("C'est la fin !");
+		// HelloWorld newHelloWorld = new HelloWorld();
+		// newHelloWorld.add(5, 4);
+
+		// int a = 2;
+		int b = 5;
+		// float c = (float) b / a;
+
+		String x = "bonjour ";
+		String y = "tout le monde";
+
+		System.out.println(x + b);
 
 	}
 
