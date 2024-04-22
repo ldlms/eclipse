328639d4d46ac8b935180f36a3586971845a239d
Leo <leodelmas31@gmail.com>
2024-04-22 14:25:42 +0200

    version

---
+++ src/com/aprentissage/Longest.java
@@ -0,0 +1,17 @@
+package com.aprentissage;
+
+public class Longest {
+	public static void main(String[] args) {
+
+		String[] strs = { "flower", "flow", "flight" };
+		System.out.println(longestCommonPrefix(strs));
+	}
+
+	public static String longestCommonPrefix(String[] strs) {
+
+		String result = "";
+
+		return result;
+	}
+
+}
---
+++ src/com/aprentissage/Runtime.java
@@ -0,0 +1,8 @@
+package com.aprentissage;
+
+public class Runtime {
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("java.runtime.version"));
+	}
+}
