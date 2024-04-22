ba8a14c9566b2705f42ad59af87dbe75580ff72c
Leo <leodelmas31@gmail.com>
2024-04-22 15:28:35 +0200

    longestCommenPrefix

--- src/com/aprentissage/Longest.java
+++ src/com/aprentissage/Longest.java
@@ -8,10 +8,33 @@ public class Longest {
 	}
 
 	public static String longestCommonPrefix(String[] strs) {
+		StringBuffer result = new StringBuffer();
+		if (strs.length == 1) {
+			result.append(strs[0]);
+		}
 
-		String result = "";
-
-		return result;
+		outerLoop: for (int i = 0; i < strs[0].length(); i++) {
+			char character = strs[0].charAt(i);
+			boolean isEqual = false;
+			for (int j = 1; j < strs.length; j++) {
+				if (i > strs[j].length() - 1) {
+					break outerLoop;
+				}
+				if (character == strs[j].charAt(i)) {
+					isEqual = true;
+				} else {
+					isEqual = false;
+					break;
+				}
+			}
+			if (isEqual == true) {
+				result.append(character);
+			} else {
+				break outerLoop;
+			}
+		}
+		String resultString = result.toString();
+		return resultString;
 	}
 
 }
