e87b73779dd8168ffd8de1f98baa6d857a0e170c
Leo <leodelmas31@gmail.com>
2024-04-25 18:17:14 +0200

    enfin

--- src/com/aprentissage/ValidParentheses.java
+++ src/com/aprentissage/ValidParentheses.java
@@ -1,14 +1,15 @@
 package com.aprentissage;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class ValidParentheses {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String s = "[({(())}[()])]";
-		System.out.println(isValid(s));
+		String s = "(([]){}())";
+		System.out.println(goodValid(s));
 	}
 
 	public static boolean isValid(String s) {
@@ -22,6 +23,10 @@ public class ValidParentheses {
 				}
 				while (!arrayChar.isEmpty()) {
 					for (int i = 0; i < start.length; i++) {
+						if ((!arrayChar.contains(start[i]) && arrayChar.contains(end[i]))
+								|| (arrayChar.contains(start[i]) && !arrayChar.contains(end[i]))) {
+							return false;
+						}
 						for (int j = arrayChar.size() - 1; j >= 0; j--) {
 							if (arrayChar.get(j) == start[i]) {
 								ArrayList<Character> subList = new ArrayList<>(arrayChar.subList(j, arrayChar.size()));
@@ -30,12 +35,16 @@ public class ValidParentheses {
 								}
 								ArrayList<Character> subSubList = new ArrayList<>(
 										subList.subList(0, subList.indexOf(end[i]) + 1));
-								if (subSubList.size() == 2) {
-									arrayChar.remove(arrayChar.indexOf(end[i]));
-									arrayChar.remove(arrayChar.lastIndexOf(start[i]));
-								}
-								if (!(subSubList.size() % 2 == 0)) {
+								if (subSubList.size() % 2 != 0) {
 									return false;
+								}
+								if (subSubList.size() == 2) {
+									arrayChar.remove(j);
+									arrayChar.remove(j);
+								}
+								if (subSubList.size() > 2) {
+									arrayChar.remove(j + (subSubList.size() - 1));
+									arrayChar.remove(j);
 								}
 							}
 						}
@@ -46,5 +55,23 @@ public class ValidParentheses {
 			return false;
 		}
 		return false;
+	}
+
+	public static boolean goodValid(String s) {
+		Stack<Character> stack = new Stack<>();
+		for (Character c : s.toCharArray()) {
+			// check if open bracket -> push the same type of close bracket to stack
+			if (c == '(')
+				stack.push(')');
+			else if (c == '[')
+				stack.push(']');
+			else if (c == '{')
+				stack.push('}');
+			else if (!(!stack.isEmpty() && stack.pop() == c))
+				return false;
+
+		}
+
+		return stack.isEmpty();
 	}
 }
