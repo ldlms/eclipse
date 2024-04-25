8c904312a186d803b0512226d1e028c44bd47a2b
Leo <leodelmas31@gmail.com>
2024-04-25 14:33:28 +0200

    toujours

--- src/com/aprentissage/ValidParentheses.java
+++ src/com/aprentissage/ValidParentheses.java
@@ -1,14 +1,13 @@
 package com.aprentissage;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class ValidParentheses {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String s = "({{{{}}}))";
+		String s = "[({(())}[()])]";
 		System.out.println(isValid(s));
 	}
 
@@ -20,40 +19,20 @@ public class ValidParentheses {
 			for (char c : s.toCharArray()) {
 				arrayChar.add(c);
 			}
-			ArrayList<Character> reverseArrayChar = new ArrayList<>(arrayChar);
-			Collections.reverse(reverseArrayChar);
 			for (int i = 0; i < start.length; i++) {
 				for (int j = 0; j < arrayChar.size(); j++) {
-					if (!(arrayChar.size() % 2 == 0)) {
-						return false;
-					}
 					if (arrayChar.contains(start[i])) {
 						if (arrayChar.contains(end[i])) {
 							if (arrayChar.indexOf(start[i]) > arrayChar.indexOf(end[i])) {
 								return false;
 							}
-							int indiceFin = arrayChar.size() - reverseArrayChar.indexOf(end[i]);
-							int indiceDebut = arrayChar.indexOf(start[i]);
-							if (arrayChar.size() > 2) {
-								ArrayList<Character> checkList = new ArrayList<>(
-										arrayChar.subList(indiceDebut + 1, indiceFin - 1));
-								if (!(checkList.size() % 2 == 0)) {
-									return false;
-								}
-							}
-							if (arrayChar.size() > 2) {
-								arrayChar.remove(indiceFin - 1);
-								arrayChar.remove(indiceDebut);
 
-							} else {
-								return false;
-							}
 						}
 					}
 				}
-				return true;
 			}
-			return false;
+			return true;
 		}
+		return false;
 	}
 }
