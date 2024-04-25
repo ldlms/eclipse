ac6114d2ae6d90ab8036c12c1df75311e8439d60
Leo <leodelmas31@gmail.com>
2024-04-25 16:15:21 +0200

    toujours encoe

--- src/com/aprentissage/ValidParentheses.java
+++ src/com/aprentissage/ValidParentheses.java
@@ -13,25 +13,37 @@ public class ValidParentheses {
 
 	public static boolean isValid(String s) {
 		if (s != null && !s.isEmpty()) {
-			char[] start = { '(', '{', '[' };
-			char[] end = { ')', '}', ']' };
-			ArrayList<Character> arrayChar = new ArrayList<>();
-			for (char c : s.toCharArray()) {
-				arrayChar.add(c);
-			}
-			for (int i = 0; i < start.length; i++) {
-				for (int j = 0; j < arrayChar.size(); j++) {
-					if (arrayChar.contains(start[i])) {
-						if (arrayChar.contains(end[i])) {
-							if (arrayChar.indexOf(start[i]) > arrayChar.indexOf(end[i])) {
-								return false;
+			if (s.length() % 2 == 0) {
+				char[] start = { '(', '{', '[' };
+				char[] end = { ')', '}', ']' };
+				ArrayList<Character> arrayChar = new ArrayList<>();
+				for (char c : s.toCharArray()) {
+					arrayChar.add(c);
+				}
+				while (!arrayChar.isEmpty()) {
+					for (int i = 0; i < start.length; i++) {
+						for (int j = arrayChar.size() - 1; j >= 0; j--) {
+							if (arrayChar.get(j) == start[i]) {
+								ArrayList<Character> subList = new ArrayList<>(arrayChar.subList(j, arrayChar.size()));
+								if (!subList.contains(end[i])) {
+									return false;
+								}
+								ArrayList<Character> subSubList = new ArrayList<>(
+										subList.subList(0, subList.indexOf(end[i]) + 1));
+								if (subSubList.size() == 2) {
+									arrayChar.remove(arrayChar.indexOf(end[i]));
+									arrayChar.remove(arrayChar.lastIndexOf(start[i]));
+								}
+								if (!(subSubList.size() % 2 == 0)) {
+									return false;
+								}
 							}
-
 						}
 					}
 				}
+				return true;
 			}
-			return true;
+			return false;
 		}
 		return false;
 	}
