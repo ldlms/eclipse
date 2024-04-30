9a5d428b7aa8eb0fba78d04eaece4b80106fdcd6
Leo <leodelmas31@gmail.com>
2024-04-30 18:16:26 +0200

    stairs

--- src/com/aprentissage/Stairs.java
+++ src/com/aprentissage/Stairs.java
@@ -3,7 +3,7 @@ package com.aprentissage;
 public class Stairs {
 
 	public static void main(String[] args) {
-		int n = 45;
+		int n = 6;
 		System.out.println(climStairs(n));
 
 	}
@@ -19,6 +19,18 @@ public class Stairs {
 		// }
 
 		// en fait c'est une suite de fibonacci
+
+		if (n == 1)
+			return 1;
+		int[] dp = new int[n + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+
+		return dp[n];
 
 	}
 
